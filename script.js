1afcca8300e7a639ec04ed408f683097c8eab8dc
SHUBAM KUMAR <112201396+shubam458@users.noreply.github.com>
2023-12-29 16:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const extendHex = (shortHex) => {
   // write your code here
 
-  let output = "#";
-  let i = 1; // Initialize i to 1 to start from the second character of the string
-  while (i <= shortHex.length - 1) { // Change the condition here
-    output += shortHex[i] + shortHex[i];
-    i++;
-  }
-  return output;
-};
+ let i = shortHex.length - 3;
+
+	let isCapital = false;
+
+	let output = "#";
+
+	while(i < shortHex.length){
+
+		let assciiCode = shortHex[i]. charCodeAt(0);
+
+		if(assciiCode >= 65 && assciiCode <=90){
+
+			isCapital = true;
+		}
+
+		output += shortHex[i] + shortHex[i];
+
+		i++;
+	}
+
+
+	return isCapital ? output.UpperCase() : output;
+	
 	
 
 // Do not change the code below.
